df594cfd3deb2896c6d46c414135b65bd1b2e329
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2024-02-22 14:19:09 -0800

    Finished Week 5 assignment

--- Assets/Scripts/Traps/LaserWall/LaserEmitter.cs
+++ Assets/Scripts/Traps/LaserWall/LaserEmitter.cs
@@ -1,15 +1,22 @@
-using System;
 using UnityEngine;
 
 public class LaserEmitter : MonoBehaviour, IShootable
 {
     private LaserWallTrap _laserWallTrap;
+    private Renderer _laserEmitterRenderer;
     
     private void Awake()
     {
         _laserWallTrap = GetComponentInParent<LaserWallTrap>();
+        _laserEmitterRenderer = GetComponentInChildren<Renderer>();
+        _laserWallTrap.DamageLevelChanged += DamageLevelChanged;
     }
-    
+
+    private void DamageLevelChanged(Color damageColor)
+    {
+        _laserEmitterRenderer.material.color = damageColor;
+    }
+
     public void Shot()
     {
         _laserWallTrap.LaserEmitterShot();
--- Assets/Scripts/Traps/LaserWall/LaserWallTrap.cs
+++ Assets/Scripts/Traps/LaserWall/LaserWallTrap.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LaserWallTrap : MonoBehaviour
@@ -7,22 +8,36 @@ public class LaserWallTrap : MonoBehaviour
     [SerializeField] private float _lasweWallUpTime;
     [SerializeField] private float _laserWallDownTime;
     [SerializeField] private float _laserWallFixTime;
-
+    [SerializeField, Min(0)] private int _damageLevels = 2;
+    private int _currentDamageLevel = 0;
+    [SerializeField] private List<Color> _damageColors;
+    
     private GameObject _laserGate;
     
     public Action LaserEmitterShot;
+    public Action<Color> DamageLevelChanged;
 
     private void Awake()
     {
-        LaserEmitterShot += LaserEmitterDown;
+        LaserEmitterShot += WallLaserEmitterShot;
 
         _laserGate = GetComponentInChildren<LaserWall>().gameObject;
+
+        _damageColors ??= new List<Color>() { Color.blue };
+        
+        DamageLevelChanged?.Invoke(_damageColors[0]);
     }
 
-    private void LaserEmitterDown()
+    private void  WallLaserEmitterShot()
     {
         if(!_laserGate.activeSelf) return;
         
+        _currentDamageLevel++;
+        DamageLevelChanged?.Invoke(_damageColors[Mathf.Min(_damageColors.Count - 1, _currentDamageLevel)]);
+        Debug.Log($"Laser Wall damage level {_currentDamageLevel}/{_damageLevels}");
+        
+        if (_currentDamageLevel < _damageLevels) return;
+        DamageLevelChanged?.Invoke(_damageColors[Mathf.Min(_damageColors.Count - 1, _currentDamageLevel)]);
         StartCoroutine(FixGate());
     }
 
@@ -30,8 +45,15 @@ public class LaserWallTrap : MonoBehaviour
     {
         _laserGate.SetActive(false);
 
-        yield return new WaitForSeconds(_laserWallFixTime);
+        while (_currentDamageLevel != 0)
+        {
+            yield return new WaitForSeconds(_laserWallFixTime/_damageLevels);
+            _currentDamageLevel--;
+            DamageLevelChanged?.Invoke(_damageColors[_currentDamageLevel]);
+        }
         
+        _currentDamageLevel = 0;
+        DamageLevelChanged?.Invoke(_damageColors[_currentDamageLevel]);
         _laserGate.SetActive(true);
     }
 }
